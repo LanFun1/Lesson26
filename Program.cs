c2e92aba6048cba4c61c9890bc47695a942f708b
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-12 18:24:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите символ, из которого хотите сделать прямоугольник");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            string rectangleSide = "";
+            for (int i =0; i< name.Length+2; i++)
+                 rectangleSide += symbol;
+            Console.WriteLine($"{rectangleSide}\n{symbol}{name}{symbol}\n{rectangleSide}");
+        }
+    }
+}
